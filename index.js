49c6ce6d74f1a95bbcc4d86c18c52421fd4a3f26
sebasBetancourt <s3basbetan@gmail.com>
2025-07-16 13:03:07 -0500

    feat: :sparkles: Add index.js

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const CompaneraCuriosa = require("./classes/CompaneraCuriosa")
 const EstudiateEspia = require("./classes/EstudianteEspia")
 
 
+const Chismosas = [
+    new TiaVecina("Claudia", 3, 5),
+    new TiaVecina("Esteban", 6, 2),
+    new CompaneraCuriosa("Acercando", 1, 8),
+    new TiaVecina("Daniel", 5, 5),
+    new EstudiateEspia("Sebas", 2, 2),
+]
+
+Chismosas.forEach((chismosa)=>{
+    chismosa.recolectarInfo()
+    chismosa.contarChisme()
+})
